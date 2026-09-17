597da53a938485002fc43ce70216ffa590810d39
dsad233 <ksl1421@naver.com>
2026-09-17 15:56:23 +0900

    fix: vite.config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3017/api",
+        target: "http://localhost:3000/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
